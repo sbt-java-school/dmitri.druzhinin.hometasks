b15e4d9c635566cd5e420b5f655f1ed22679ac67
Dima <dima.cba@yandex.ru>
2016-09-10 00:27:47 +0700

    hometask11 Уникальность плагина определяется названием плагина и именем класса.

--- hometask11/src/main/java/ru/sbt/PluginManager.java
+++ hometask11/src/main/java/ru/sbt/PluginManager.java
@@ -10,7 +10,8 @@ import java.util.Map;
 public class PluginManager {
     private final String pluginRootDirectory;
     private final PluginLoader pluginLoader;
-    private final Map<String, Class<?>> loadedClasses;//cache.
+    private final Map<UniquePluginName, Class<?>> loadedClasses;//cache.
+
 
     public PluginManager(String pluginRootDirectory) throws MalformedURLException {
         this.pluginRootDirectory = pluginRootDirectory;
@@ -19,15 +20,17 @@ public class PluginManager {
     }
 
     public Plugin load(String pluginName, String pluginClassName) throws MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class<?> pluginClass=loadedClasses.get(pluginClassName);
+        UniquePluginName name=new UniquePluginName(pluginName, pluginClassName);
+        Class<?> pluginClass=loadedClasses.get(name);
         if(pluginClass == null) {//если раньше такой класс не загружался в этом pluginManager
             pluginLoader.addURL(new URL("file://" + pluginRootDirectory + "/" + pluginName + "/"));
             pluginClass = pluginLoader.loadClass(pluginClassName);
-            loadedClasses.put(pluginClassName, pluginClass);
+            loadedClasses.put(name, pluginClass);
         }
         return (Plugin) pluginClass.newInstance();
     }
-     private class PluginLoader extends URLClassLoader{
+
+    private class PluginLoader extends URLClassLoader{
 
         public PluginLoader(URL[] urls, ClassLoader parent) {
             super(urls, parent);
@@ -54,6 +57,39 @@ public class PluginManager {
         @Override
         protected void addURL(URL url){
             super.addURL(url);
+        }
+    }
+
+    /**
+     * Плагин считается уникальным, даже если имя его класса неуникально. Поэтому PluginManager кэширует не по имени класса,
+     * а по названию плагина и имени класса.
+     */
+    static private class UniquePluginName{
+        private final String plugineName;
+        private final String pluginClassName;
+
+        private UniquePluginName(String plugineName, String pluginClassNamel) {
+            this.plugineName = plugineName;
+            this.pluginClassName = pluginClassNamel;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            UniquePluginName that = (UniquePluginName) o;
+
+            if (plugineName != null ? !plugineName.equals(that.plugineName) : that.plugineName != null) return false;
+            return pluginClassName != null ? pluginClassName.equals(that.pluginClassName) : that.pluginClassName == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = plugineName != null ? plugineName.hashCode() : 0;
+            result = 31 * result + (pluginClassName != null ? pluginClassName.hashCode() : 0);
+            return result;
         }
     }
 }
