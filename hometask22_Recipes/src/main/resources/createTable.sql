94f7eb9b1075198e66c801bcc3f9038b1a409219
Dima <dima.cba@yandex.ru>
2016-11-08 00:56:25 +0700

    merge

--- hometask22_Recipes/src/main/resources/createTable.sql
+++ hometask22_Recipes/src/main/resources/createTable.sql
@@ -1,16 +1,16 @@
 create table if not exists recipes(
-  name varchar2(20) not null,
-  description varchar2(20),
+  name varchar2(50) not null,
+  description varchar2(50),
   constraint pk_recipe_name primary key (name)
 );
 create table if not exists ingredients(
-  name varchar2(20) not null,
-  measure_unit varchar2(20) not null,
+  name varchar2(50) not null,
+  measure_unit varchar2(50) not null,
   constraint pk_ingredient_name primary key (name)
 );
 create table if not exists ingredients_of_recipes(
-  rname varchar2(20) not null references recipes(name),
-  iname varchar2(20) not null references ingredients(name),
+  rname varchar2(50) not null references recipes(name),
+  iname varchar2(50) not null references ingredients(name),
   amount integer not null default 0,
 
   foreign key(rname) references recipes(name) on delete restrict,
