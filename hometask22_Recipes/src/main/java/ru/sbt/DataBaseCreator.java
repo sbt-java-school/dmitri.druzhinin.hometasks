7e94f2ebf708b76be80f5a30ef4b37c88c64f0f9
Dima <dima.cba@yandex.ru>
2016-11-08 14:42:39 +0700

    hometask22, снял публичность с членов классов где она не нужна.

--- hometask22_Recipes/src/main/java/ru/sbt/DataBaseCreator.java
+++ hometask22_Recipes/src/main/java/ru/sbt/DataBaseCreator.java
@@ -11,20 +11,20 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-public class DataBaseCreator {
+class DataBaseCreator {
     private static Logger logger = LoggerFactory.getLogger(DataBaseCreator.class);
     private JdbcTemplate jdbcTemplate;
 
-    public DataBaseCreator(JdbcTemplate jdbcTemplate) {
+    DataBaseCreator(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public static void createDatabase() {
+    static void createDatabase() {
         ApplicationContext context = new ClassPathXmlApplicationContext("app-context-xml-database-creator.xml");
         context.getBean(DataBaseCreator.class).executeScript("/createTable.sql");
     }
 
-    public void executeScript(String fileName) {
+    private void executeScript(String fileName) {
         try {
             URL sqlResource = DataBaseCreator.class.getResource(fileName);
             String sqlScript = FileUtils.readFileToString(new File(sqlResource.getFile()));
